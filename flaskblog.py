6ac603f703d2858a6f167e23be535b1f2f78a446
= <lostipod12@gmail.com>
2023-10-06 17:43:46 -0700

    commit right before heafing off to church concert

---
+++ flaskblog.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+
+posts = [
+	
+        {
+			'author': 'Nick White',
+			'title': 'Blog Post 1',
+			'content': 'First Post content',
+			'date_posted': 'April 20, 2018'
+
+        },
+
+        {
+			'author': 'Your Mom',
+			'title': 'Your Mom Went To college',
+			'content': 'haha haaha',
+			'date_posted': 'April 16, 2019'
+
+        }
+
+
+
+]
+
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+	return render_template("about.html")
+
+if __name__ == "__main__":
+	app.run(debug=True)
