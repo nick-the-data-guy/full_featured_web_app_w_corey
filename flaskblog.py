3900162da257a0b8d82e2a0adc2345ea94b54f30
= <lostipod12@gmail.com>
2023-10-09 10:29:44 -0700

    finished video 2, Templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app=Flask(__name__)
 
@@ -34,7 +34,7 @@ def home():
 
 @app.route("/about")
 def about():
-	return render_template("about.html")
+	return render_template("about.html", title='About')
 
 if __name__ == "__main__":
 	app.run(debug=True)
